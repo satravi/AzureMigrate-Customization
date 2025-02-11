74c8fee4a3f08c12eddd0da6ff6d38c6bb50caca
Sathya Prabhu Ravi <96466317+satravi@users.noreply.github.com>
2025-02-11 17:58:25 +0530

    Update UsingManagedIdentity.ps1

--- Runbooks/Test/UsingManagedIdentity.ps1
+++ Runbooks/Test/UsingManagedIdentity.ps1
@@ -1,4 +1,13 @@
-"Please enable appropriate RBAC permissions to the system identity of this automation account. Otherwise, the runbook may fail..."
+<#
+    .DESCRIPTION
+        An example runbook which gets all the ARM resources using the Managed Identity
+
+    .NOTES
+        AUTHOR: Azure Automation Team
+        LASTEDIT: Oct 26, 2021
+#>
+
+"Please enable appropriate RBAC permissions to the system identity of this automation account. Otherwise, the runbook may fail...""Please enable appropriate RBAC permissions to the system identity of this automation account. Otherwise, the runbook may fail..."
 
 try
 {
